afa4f46ab44264614015467acad14ee7af99c81b
Deepak Kokani <103135934+deepakkokani@users.noreply.github.com>
2024-02-26 13:35:23 +0530

    first.js

--- first.js
+++ first.js
@@ -1,12 +1,36 @@
-let guessnumber = 10;
 let number;
-for(i=1;i<=10;i++){
-    number = prompt("Guess The Number");
-    if(number==guessnumber){
-        alert("Congratulations well Played");
-        break;
+const generate_number=()=>{
+    let n1 = Math.random();
+    let n2 = n1*10;
+    n2 = Math.trunc(n2);
+    n2 = ( n2 % 10 )+1;
+    number = n2
+    console.log(number);
+}
+
+//generate_number();
+
+const generate_number_btn_click = () =>{
+    document.querySelector("#div1").style.display="none";
+    document.querySelector("#div2").style.display="none";
+    document.querySelector("#div3").style.display="block";
+    setTimeout(()=>{
+        generate_number();
+        document.querySelector("#div1").style.display="none";
+        document.querySelector("#div2").style.display="block";
+        document.querySelector("#div3").style.display="none";
+    }, 3000)
+};
+
+//check number
+const check_number = () =>{
+    let value = document.querySelector("#field").value;
+    if(value==number)
+    {
+        alert("Congratulations you won the game");
     }
     else{
-        alert("Try Again");
+        alert("OOPS !! your guess is wrong");
     }
+    generate_number_btn_click();
 }
